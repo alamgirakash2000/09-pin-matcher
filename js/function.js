d5ec3874218fca6422d733cb2bfcbf16497644f9
Md. Alamgir Hossain <alamgirakash2000@gmail.com>
2020-07-27 14:04:43 +0600

    null correction

--- js/function.js
+++ js/function.js
@@ -5,6 +5,12 @@ function generatePin(){
     var pin=1000+ Math.floor(Math.random()*9000)
     displayGenerated.value= pin
     submitBtn.removeAttribute('disabled')
+    leftTries=3
+    displayGuessed.value=''
+    document.getElementById("left-tries").className='action-left d-block'
+    document.getElementById("notMatchedMessage").className='notify d-none'
+    document.getElementById("matchedMessage").className ='notify d-none'
+    document.getElementById("left-tries").innerText=`${leftTries} try left`
 }
 
 // Show and set the guessed pin
@@ -16,7 +22,9 @@ function getSetPin(e){
 
 // Function for the clicking of the submit button
 function submitFunction(){
-    if( displayGuessed.value === displayGenerated.value){
+    if(displayGenerated.value===""){
+        alert('Generate a pin first')
+    }else if(displayGuessed.value === displayGenerated.value){
         document.getElementById("left-tries").className='action-left d-none'
         document.getElementById("notMatchedMessage").className='notify d-none'
         document.getElementById("matchedMessage").className ='notify d-block'
